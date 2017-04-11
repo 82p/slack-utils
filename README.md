6cee0f6a02f44b48c972a3d9812dd5d821f2a530
kikuchi_kentaro <kikuchi_kentaro@sgk.jp>
2017-04-11 15:15:51 +0900

    add script to invite users to times_XXX channels

---
+++ README.md
@@ -0,0 +1,15 @@
+# Slack Utility Tools
+
+## invite to times
+
+this script is invite all users to all 'times_XXX' channel.
+
+```bash
+set SLACK_API_TOKEN="token"
+python invite_to_times.py
+```
+
+### what 'times_XXX' channel ?
+
+[Refer here](http://c16e.com/1511101558/)
+
---
+++ invite_to_times.py
@@ -0,0 +1,32 @@
+import os
+from slackclient import SlackClient
+
+TOKEN = os.environ["SLACK_API_TOKEN"]
+
+
+def invite_to_times():
+    """invite all users to all 'times_XXX' channels"""
+
+    if TOKEN:
+        sc = SlackClient(TOKEN)
+
+        # get users
+        users = sc.api_call("users.list")
+
+        # get channels and invite all users
+        channels = sc.api_call("channels.list", exclude_archived=True)
+        for ch in channels["channels"]:
+            # only 'times_XXX' channels
+            if ch["name"].startswith("times_"):
+                for user in users["members"]:
+                    # only human
+                    if user["name"] != "slackbot" and not user["is_bot"]:
+                        res = sc.api_call("channels.invite", channel=ch["id"], user=user["id"])
+
+                        if res["ok"]:
+                            print("{} added to {}".format(user["name"], ch["name"]))
+    else:
+        print("slack api token is not provided")
+
+if __name__ == '__main__':
+    invite_to_times()
